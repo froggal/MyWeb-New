691810f238196d442e8521986378ee450cf5e4ff
froggal <doeon111@naver.com>
2022-07-16 14:11:48 +0900

    revert

--- src/script.js
+++
@@ -1,7 +0,0 @@
-// 클릭시 정확한 위치로 이동
-
-function locationTo(dom) {
-    let target = document.querySelector(dom);
-    const clientRect = window.pageYOffset + target.getBoundingClientRect().top;
-    window.scrollTo(0, (clientRect-50));
-}
